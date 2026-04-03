643e38b24821a4604055e220479643c14ff3fd7d
dharmppp21 <daddugaming16@gmail.com>
2026-04-04 04:41:09 +0530

    Added basic animations

---
+++ Hackbyte4.0/Assets/Models/Character/Scripts/PlayerMove.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float walkSpeed = 3f;
+    public float runSpeed = 6f;
+    public float rotationSpeed = 10f;
+    public float jumpForce = 6f;
+
+    private Animator animator;
+    private Rigidbody rb;
+    private PlayerInput playerInput;
+
+    private InputAction moveAction;
+    private InputAction runAction;
+    private InputAction jumpAction;
+
+    private Vector2 moveInput;
+    private bool isGrounded = true;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
+
+        moveAction = playerInput.actions["Move"];
+        runAction = playerInput.actions["Run"];
+        jumpAction = playerInput.actions["Jump"];
+    }
+
+    void Update()
+    {
+        moveInput = moveAction.ReadValue<Vector2>();
+        bool isRunning = runAction.IsPressed();
+
+        Vector3 move = new Vector3(moveInput.x, 0f, moveInput.y);
+
+        if (move.magnitude > 0.1f)
+        {
+            float currentSpeed = isRunning ? runSpeed : walkSpeed;
+            transform.Translate(move.normalized * currentSpeed * Time.deltaTime, Space.World);
+
+            Quaternion targetRotation = Quaternion.LookRotation(move);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+
+        float animSpeed = 0f;
+        if (moveInput.magnitude > 0.1f)
+            animSpeed = isRunning ? 1f : 0.5f;
+
+        animator.SetFloat("Speed", animSpeed, 0.1f, Time.deltaTime);
+        animator.SetBool("IsGrounded", isGrounded);
+
+        if (jumpAction.triggered && isGrounded)
+        {
+            isGrounded = false;
+            animator.SetBool("IsGrounded", false);
+            animator.SetTrigger("Jump");
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            animator.SetBool("IsGrounded", true);
+        }
+    }
+}
